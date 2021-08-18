d8da178f0bb88b28183b6e73c9104ae6dd83d3cf
jerke godeke <ekrej@outlook.com>
2021-08-18 17:49:27 +0200

    start on adding games to list

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +14,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using GameSelector.Models;
+using Selector = GameSelector.Controllers.Selector;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,14 +26,37 @@ namespace GameSelector
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private Selector Selector { get; set; }
+        public GamesCollection Games = new GamesCollection();
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            ApplicationView.PreferredLaunchViewSize = new Size(1096, 1000);
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+
+            Selector = new Selector();
         }
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
             GridShadow.Receivers.Add(BackgroundGrid);
+        }
+
+        private void AddButtonClick(object sender, RoutedEventArgs e)
+        { 
+            Games.AddGame();
+        }
+
+        private void DeleteButtonClick(object sender, RoutedEventArgs e)
+        {
+            //ee
+        }
+
+        private void SelectButtonClick(object sender, RoutedEventArgs e)
+        {
+            //ee
         }
     }
 }
---
+++ Models/GamesCollection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameSelector.Models
+{
+    public class GamesCollection: ObservableCollection<Game>
+    {
+        public GamesCollection()
+        {
+            Add( new Game("Add a Game"));
+        }
+
+        public void AddGame() => Add(new Game("New Game"));
+    }
+}

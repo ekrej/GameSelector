2f8ed79f2bea12cd2b5e1d9259353461d1e513f6
jerke godeke <ekrej@outlook.com>
2021-08-31 17:39:04 +0200

    trying out new button layouts

--- WPF/MainWindow.xaml.cs
+++ WPF/MainWindow.xaml.cs
@@ -24,5 +24,18 @@ namespace GameSelector
         {
             InitializeComponent();
         }
+
+        private void Minimize_Click(object sender, RoutedEventArgs e) 
+            => Application.Current.MainWindow.WindowState = WindowState.Minimized;
+
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+            => Application.Current.Shutdown();
     }
 }
